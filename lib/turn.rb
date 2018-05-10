8229ecd3babab98a90a46d66c464791e5d6e810b
Vicki Hamm <vicki.cello@gmail.com>
2018-05-10 22:09:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(boardArray, index, value = "X")
+  boardArray [index] = value
+end
+
+
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil 
     false
