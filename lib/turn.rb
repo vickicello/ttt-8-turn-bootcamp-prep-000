85f68156e2814ebdbeb9f1aecb27ed3cabc6e120
Vicki Hamm <vicki.cello@gmail.com>
2018-05-10 22:04:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,5 +3,15 @@ def position_taken?(board, index)
     false
   else 
     true
+ end
 end
+
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    false
+  elsif !(index.between?(0, 8))
+    false
+  else 
+    true
+ end
 end
